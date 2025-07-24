bcd6a6b24655b19ddccf1790b21ef73576a57cce
Aman raj <129151243+aman18raj@users.noreply.github.com>
2025-07-24 10:21:25 +0530

    Add files via upload

---
+++ collectionDemo.java
@@ -0,0 +1,14 @@
+public class collectionDemo {
+    public static void main(String[] args) {
+       /*  HashMap<String, Integer> songs = new HashMap<>();
+        phone.put("ram",2222);
+
+        HashSet<String> songs = new HashSet<>();
+        songs.add("Bang Bang");
+        songs.add("Boom Boom");
+        songs.add("Bang Bang");
+        System.out.println(songs);
+        */
+    }
+    
+}
