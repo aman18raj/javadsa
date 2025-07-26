50cb7114f310c1bf623b2b5032506e74e5afd0ac
Aman raj <129151243+aman18raj@users.noreply.github.com>
2025-07-26 11:46:16 +0530

    Add files via upload

---
+++ diceboard.java
@@ -0,0 +1,12 @@
+public class diceboard {
+    static void diceBoard(int start, int end, int result ){
+        if(start == end){
+             
+
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
