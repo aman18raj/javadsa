424e714ff1cc0a5903c310521cb10bbc083ab518
Aman raj <129151243+aman18raj@users.noreply.github.com>
2025-08-07 12:47:03 +0530

    Add files via upload

---
+++ colorSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class colorSort {
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+        System.out.println("Sorted array: " + Arrays.toString(nums));
+    }
+}
